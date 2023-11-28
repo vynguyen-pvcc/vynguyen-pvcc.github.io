5f24ab8c908e4f58b97ba2ff8044ba188a2a9b72
V Nguyen <149738624+vynguyen-pvcc@users.noreply.github.com>
2023-11-28 17:12:47 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Name: V Nguyen
+# Prog Purpose: Hello World
+
+print("Hello world! This is my first Python program.")
+print("I can program in HTML, which is awesome.")
+print("However, HTML cannot do arithmetic.")
+print("But Python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("and lots of other things!")
